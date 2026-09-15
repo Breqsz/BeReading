f383538f4b6d413439185e9769da3a5ce1dac2ed
Breq1337 <guirochabianchini@gmail.com>
2026-09-15 17:25:19 -0300

    feat(BER-77): conversa do quiz com bolhas e composer

--- mobile/src/features/quiz-chat/index.ts
+++ mobile/src/features/quiz-chat/index.ts
@@ -1,2 +1,5 @@
+export { ChatBubble } from './ChatBubble';
+export { Composer } from './Composer';
+export { QuizConversation } from './QuizConversation';
 export { buildConversation, scoreTagLabel, answeredXp } from './logic';
 export type { ChatMessage, ConversationInput } from './logic';
